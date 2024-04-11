e1bb1c2537479364868006638f16b7e9f24382da
Shantanu29kumar <shantanukumar7602@gmail.com>
2024-04-11 17:41:53 +0530

    First Commit

--- train_classifier.py
+++ train_classifier.py
@@ -1,28 +1,3 @@
-#
-# import pickle
-# import numpy as np
-#
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.model_selection import train_test_split
-# from sklearn.metrics import accuracy_score
-#
-#
-# data_dict = pickle.load(open('D:\SignLanguageProject\SILT\data.pickle','rb'))
-#
-# data = np.asarray(data_dict['data'])
-# labels = np.asarray(data_dict['labels'])
-#
-#
-# x_train,x_test,y_train,y_test = train_test_split(data,labels,test_size=0.2,shuffle=True,stratify=labels)
-#
-# model = RandomForestClassifier()
-#
-# model.fit(x_train,y_train)
-#
-# y_pred = model.predict(x_test)
-#
-#
-
 #main code
 
 import pickle
